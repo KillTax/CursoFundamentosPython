f6ce481c046f40fc69e803e9303782975b8c2f87
Max <maxvega091@gmail.com>
2023-12-13 23:40:37 -0600

    Fin de clase 2

---
+++ clase_2_variables.py
@@ -0,0 +1,14 @@
+my_name = "Max" #Variable de tipo string
+my_age = 25 #Variable de tipo entero
+
+print("Mi nombre es: " + my_name)
+print("Mi edad es: " + str(my_age))
+
+my_name = "Daniel" #Se puede cambiar el valor de las variables
+print("Mi nombre es: " + my_name)
+
+#Pedimos al usuario que ingrese los datos con la funcion input
+
+my_name = input("Cual es tu nombre?")
+
+print("El nombre que me diste es :" + str(my_name))
