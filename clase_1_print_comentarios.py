41231c695530a19716cf6cd6329942aa5bda2b34
Max <maxvega091@gmail.com>
2023-12-13 23:22:20 -0600

    primer commit

---
+++ clase_1_print_comentarios.py
@@ -0,0 +1,29 @@
+#Imprimer en terminal informacion proporcionada
+print("Hola, esto es python")
+
+print("Soy Max y tengo 25 años")
+
+# Puedes hacer operaciones matematicas
+print(12 + 5)
+print(10 - 5)
+print(12*3)
+print(8/4)
+
+# Tiene errores al momento de identar mal
+
+"""
+Comentario
+de
+varias
+lineas
+"""
+
+'''
+Tambien
+se
+puede
+comentar
+con
+comillas
+simples
+'''
